07bcf2e430a157bcea4fbdf2e454abc0ab0e9ac6
Mohammed Ajmal <hubblesbury449@gmail.com>
2026-06-25 00:02:46 +0530

    updated __init__.py files for easier re-usable imports in the future

---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,9 @@
+from .users import UserRegister, UserLogin, UserOut, Token
+
+
+from .interntrack import (
+    CVVersionOut,
+    ApplicationOut,
+    ApplicationCreate,
+    ApplicationUpdate
+)
---
+++ backend/app/schemas/interntrack/__init__.py
@@ -0,0 +1,5 @@
+from .cv_version_out import CVVersionOut
+from .application_base import ApplicationBase
+from .application_create import ApplicationCreate
+from .application_update import ApplicationUpdate
+from .application_out import ApplicationOut
